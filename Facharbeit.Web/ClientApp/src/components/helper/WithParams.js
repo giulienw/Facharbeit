d0e0b4f1b2dde9b1825527c8093dc53b80651a65
Giulien Chow <52256086+giulienw@users.noreply.github.com>
2023-02-09 12:12:15 +0100

    fixed docker

--- Facharbeit.Web/ClientApp/src/components/helper/WithParams.js
+++
@@ -1,6 +0,0 @@
-import React, { Component } from "react";
-import { useParams } from "react-router-dom";
-
-export default function withParams (Component) {
-    return props => <Component {...props} params={useParams()} />;
-};
